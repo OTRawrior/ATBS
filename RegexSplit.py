ee12bbc95ae44c46f4fde2196884352fc01fb14c
Reece Powell <reece.powell95@gmail.com>
2018-10-18 21:11:01 +0100

    Added strong password detector & started regex split (chapter 7 projects).

---
+++ RegexSplit.py
@@ -0,0 +1,16 @@
+"""Write a function that takes a string and does the same thing as the strip() string method. If no other arguments
+are passed other than the string to strip, then whitespace characters will be removed from the beginning and end of
+the string. Otherwise, the characters specified in the second argument to the function will be removed from the
+string. """
+
+# ! python2
+
+import re
+
+string = '    some   texty stuff  '
+
+
+def regex_split(toSplit):
+    toSplit = 5
+
+
--- StrongPasswordDetection.py
+++ StrongPasswordDetection.py
@@ -1,26 +1,29 @@
 #! python3
-'''Write a function that uses regular expressions to make sure the password 
-string it is passed is strong. A strong password is defined as one that is at 
-least eight characters long, contains both uppercase and lowercase characters, 
-and has at least one digit. You may need to test the string against multiple 
-regex patterns to validate its strength.'''
-
+"""Write a function that uses regular expressions to make sure the password
+string it is passed is strong. A strong password is defined as one that is at
+least eight characters long, contains both uppercase and lowercase characters,
+and has at least one digit. You may need to test the string against multiple
+regex patterns to validate its strength."""
 
 import re
 
-#password = input('Please enter your password: ')
+password = input('Please enter your password: ')
 
-strongLength = re.compile(r'''(
-        
+strongLength = re.compile(r'.{8}')
 
-)''', re.VERBOSE)
+strongUpper = re.compile(r'[A-Z]')
 
-strongCases = re.compile(r'''(
-        
+strongLower = re.compile(r'[a-z]')
 
-)''', re.VERBOSE)
+strongDigits = re.compile(r'\d')
 
-strongDigits = re.compile(r'''(
-        
-
-)''', re.VERBOSE)
+if strongLength.search(password) != None:
+    if strongUpper.search(password) != None and strongLower.search(password) != None:
+        if strongDigits.search(password) != None:
+            print(password + ' is a strong password.')
+        else:
+            print('Passwords must contain at least one digit.')
+    else:
+        print('Passwords must contain both upper and lower case characters.')
+else:
+    print('Passwords must be at least 8 characters.')
