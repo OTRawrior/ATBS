6a5ff44b7cc2178fd76687608386e87ab48ebf02
Reece Powell <reece.powell95@gmail.com>
2018-10-22 19:55:00 +0100

    Added ZipBackup and Amer2Euro dates, and ATBS proj files.

---
+++ ZipFileBackup.py
@@ -0,0 +1,19 @@
+#! python 3
+# Created by OTRawrior
+# Function: [what is it supposed to be for]
+# Usage: [how its run, e.g. py file.py arg1 arg2]
+
+# Modules
+import shutil
+import os
+
+
+# Functions
+def main():
+    # main code
+
+    return
+
+
+if __name__ == '__main__':
+    main()
